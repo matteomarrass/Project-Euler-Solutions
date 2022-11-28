6670f47734b8e5048afff8d50bb8870a387716cf
Matteo Marras <matteommarras@gmail.com>
2022-11-28 10:27:00 +0100

    Project Euler First Problem - Solution

---
+++ home.js
@@ -0,0 +1,19 @@
+/* Project Euler Solutions - Problem 1
+
+console.log('hello')
+
+let total = 0
+
+const calculateMultiply = () => {
+    for(let i = 1; i < 1000; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            total += i
+        }
+    }
+
+}
+
+calculateMultiply();
+console.log(total)
+
+*/
